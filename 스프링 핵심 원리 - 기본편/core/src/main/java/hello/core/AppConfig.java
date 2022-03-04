8ee97746cda467ea10e8a293f025adaad16598cc
soyeon-nam <soyeonlnam@gmail.com>
2022-03-04 16:40:37 +0900

    refactor : using spring instead of pure java

--- 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AppConfig.java
+++ 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AppConfig.java
@@ -9,21 +9,28 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration // 구성정보
 public class AppConfig {
 
+	@Bean //각 메서드에 @bean 을 적어주면 스프링 컨테이너에 등록됨
 	public MemberService memberService() {
 		return new MemberServiceImpl(memberRepository());
 	}
 
-	private MemberRepository memberRepository() {
+	@Bean
+	public MemberRepository memberRepository() {
 		return new MemoryMemberRepository();
 	}
 
+	@Bean
 	public OrderService orderService() {
 		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
 
+	@Bean
 	public DiscountPolicy discountPolicy() {
 		return new RateDiscountPolicy();
 	}
